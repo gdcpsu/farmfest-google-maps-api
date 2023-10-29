dac31c0288d66b1410d2ebc931e1778fdf5c3a92
gdcpsu <147957211+gdcpsu@users.noreply.github.com>
2023-10-29 17:15:24 -0500

    Create maps.php

---
+++ maps.php
@@ -0,0 +1,14 @@
+[av_google_map zoom_control='aviaTBzoom_control' maptype_control='' streetview_control='aviaTBstreetview_control' zoom='5' maptype_id='HYBRID' height='500px' saturation='' hue='' google_link='' confirm_button='Click to load Google Maps' page_link_text='Open Google Maps in a new window' google_fallback='' attachment='' attachment_size='' id='' custom_class='' template_class='' av_uid='av-lo66mvii' sc_version='1.0']
+[av_gmap_location address='' postcode='' city='iowa city' state='iowa' country='usa' long='-91.53461689999999' lat='41.6578257' marker='' imagesize='40' av_uid='av-lo66mtyw' sc_version='1.0' tooltip_display='']
+Iowa Farmer
+Carrots
+Apples
+Pumpkins
+[/av_gmap_location]
+[av_gmap_location address='' postcode='60134' city='geneva' state='il' country='usa' long='-88.3336477' lat='41.8778243' marker='' imagesize='40' av_uid='av-lo66u7zh' sc_version='1.0' tooltip_display='']
+Geneva Farms
+Honey
+Apples
+[/av_gmap_location]
+[/av_google_map]
+
